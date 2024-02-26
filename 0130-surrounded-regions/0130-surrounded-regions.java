04a88c9ab1525b2726109ab17eadca1496876094
gbtwld <gbtwld@gmail.com>
2024-02-26 17:30:18 +0900

    Time: 22 ms (5.14%), Space: 45.5 MB (35.04%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] dx = new int[] { 0, -1, 0, 1 };
+    public int[] dy = new int[] { -1, 0, 1, 0 };
+
+    public void dfs(char[][] board, int i, int j, boolean[][] visited, boolean[][] dp) {
+        dp[i][j] = true;
+        visited[i][j] = true;
+
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = i + dx[dir], ny = j + dy[dir];
+
+            if (!(nx >= 0 && ny >= 0 && nx < board.length && ny < board[0].length) || visited[nx][ny] || board[nx][ny] == 'X') continue;
+
+            dfs(board, nx, ny, visited, dp);
+        }
+    }
+
+    public void solve(char[][] board) {
+        int m = board.length, n = board[0].length;
+        char[][] origin = board.clone();
+        boolean[][] dp = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!(i == 0 || j == 0 || i == board.length - 1 || j == board[0].length - 1)) continue;
+                if (board[i][j] == 'X') continue;
+
+                boolean[][] visited = new boolean[m][n];
+
+                dfs(board, i, j, visited, dp);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!dp[i][j] && board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+}
